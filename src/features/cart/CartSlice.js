40de6ef2db5797a54501c221390e72a204ac79cb
Taishi1020 <tai.yakiman1020@gmail.com>
2023-10-16 23:36:19 +0900

    状態の初期値を設定

---
+++ src/features/cart/CartSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//状態の初期値
+
+const initialState = {
+    createItems: {},
+    amount: 0,
+    total: 0,
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {},
+})
+
+
+export default cartSlice.reducer;
